9e42041880cac4aa495ad11cfebbe2a8213d4722
Arturo Negreiros <anegreiross@outlook.com>
2021-02-07 02:06:01 -0500

    ok

--- java/src/com/company/Main.java
+++ java/src/com/company/Main.java
@@ -1,10 +1,93 @@
 package com.company;
 
-public class Main {
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-    public static void main(String[] args) {
-        System.out.println("Hackerrank solutions");
+public class Solution {
+
+
+
+
+/**
+ *@param grid the matrix with the positions
+ *@param n is bot position
+ *
+ * */
+static void displayPathtoPrincess(int n, String [] grid){
+    
+    ArrayList<ArrayList<String>> vector = new ArrayList<ArrayList<String>>();
+    int peachPositionW =  0;
+    int peachPositionH =  0;
+    int botPositionW = 0;
+    int botPositionH = 0;
+
+
+        for (int x = 0; x < grid.length; x++){
+            String [] source = grid[x].split("");
+            ArrayList<String> values = new ArrayList<String>();
+            for (int z = 1; z < source.length; z++){
+                values.add(source[z]);
+            }
+            vector.add(values);
+        }
+    
+    for (int i =0 ; i < vector.size(); i++){
+            
+            for (int j =0; j < vector.size(); j++){
+                
+                if (vector.get(i).get(j).equals("m")){
+                    
+                    botPositionW = j;
+                    botPositionH = i;
+                }else if (vector.get(i).get(j).equals("p")){
+
+                    peachPositionW = j;
+                    peachPositionH = i;
+                }
+            }
+        }
+    
+    if (botPositionH > peachPositionH){
+        counter = botPositionH - peachPositionH;
+        
+        for (int x = 0; x < counter; x++){
+            System.out.println("UP");
+        }
+    } else if (botPositionH < peachPositionH){
+        counter =  peachPositionH - botPositionH;
+        
+        for (int x = 0; x < counter; x++){
+            System.out.println("DOWN");
+        }
     }
+    
+    if (botPositionW > peachPositionW){
+        counter = botPositionW - peachPositionW;
+        
+        for (int x = 0; x < counter; x++){
+            System.out.println("LEFT");
+        }
+    } else if (botPositionW < peachPositionW){
+        counter =  peachPositionW - botPositionW;
+        
+        for (int x = 0; x < counter; x++){
+            System.out.println("RIGHT");
+        }
+    }
+  }
 
+public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m;
+        m = in.nextInt();
+        String grid[] = new String[m];
+        for(int i = 0; i < m; i++) {
+            grid[i] = in.next();
+        }
 
+    displayPathtoPrincess(m,grid);
+    }
 }
