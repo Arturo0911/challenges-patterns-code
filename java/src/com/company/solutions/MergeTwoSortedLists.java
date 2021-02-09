90330b12e678ac80cf19d0803ddde578ff10fb12
Arturo Negreiros <anegreiross@outlook.com>
2021-02-09 15:01:32 -0500

    ok

--- java/src/com/company/solutions/MergeTwoSortedLists.java
+++ java/src/com/company/solutions/MergeTwoSortedLists.java
@@ -11,6 +11,32 @@ public class MergeTwoSortedLists {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return l1;
+
+        ListNode root = new ListNode();
+        ListNode pointer = root;
+
+
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                pointer.next = l1;
+                l1 = l1.next;
+
+            }else{
+                pointer.next = l2;
+                l2 = l2.next;
+            }
+
+            pointer = pointer.next;
+        }
+
+        if (l1 != null){
+            pointer.next = l1;
+        }
+        if (l2 != null){
+            pointer.next = l2;
+        }
+
+
+        return root.next;
     }
 }
