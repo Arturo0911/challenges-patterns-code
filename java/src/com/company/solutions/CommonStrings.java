5445a00297fd095a920d46f8eba0660b6f5cae2e
Arturo Negreiros <anegreiross@outlook.com>
2021-02-08 19:22:16 -0500

    still finish the common strings

--- java/src/com/company/solutions/CommonStrings.java
+++ java/src/com/company/solutions/CommonStrings.java
@@ -1,5 +1,13 @@
 package com.company.solutions;
 
+import com.company.Main;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class CommonStrings {
 
     /**
@@ -9,7 +17,30 @@ public class CommonStrings {
      * */
     public String longestCommonPrefix(String[] strs) {
         int sizeString = strs.length;
+        String param = "";
+        ArrayList<Integer> vectorSize = new ArrayList<>();
+        for (String wordSizes: strs){
+            vectorSize.add(wordSizes.length());
+        }
+        Collections.sort(vectorSize);
+        int commonSize = vectorSize.get(0);
+
+
+
+
 
         return null;
+    }
+
+
+    public static void main(String[] args) {
+        CommonStrings common = new CommonStrings();
+        String [] words = {"flower", "flow", "flautarin"};
+        //System.out.println(common.longestCommonPrefix(words));
+
+        String palabra = "Arturo";
+        System.out.println(palabra.substring(0,5));
+
+
     }
 }
