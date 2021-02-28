cd1a9c160c2aa936a9b634994d184095586eeea6
Arturo Negreiros <anegreiross@outlook.com>
2021-02-28 16:37:54 -0500

    addee new ApiFetch class

---
+++ InternalTest/src/main/java/com/company/process/interview/ApiFetch.java
@@ -0,0 +1,21 @@
+package com.company.process.interview;
+
+import java.net*;
+
+
+
+public class ApiFetch {
+
+
+
+	public static void main(String[] args){
+
+
+		System.out.println("hello world");
+
+
+
+	}
+
+
+}
