ae40c14fc4fa787e6c79c229301deb3e4b364eed
Arturo Negreiros <anegreiross@outlook.com>
2021-03-05 19:20:52 -0500

    addede kangaro solutions

--- java/src/com/company/multithreading/Multithreading.java
+++ java/src/com/company/multithreading/Multithreading.java
@@ -1,8 +1,13 @@
 package com.company.multithreading;
 
+/**
+ * @author Arturo Negreiros
+ */
+
+
 public class Multithreading {
 
     public static void main(String[] args) {
-        
+
     }
 }
