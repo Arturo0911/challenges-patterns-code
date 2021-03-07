9135707c7574cebe4523eddfe884d1f15b75dbf9
Arturo0911 <anegreiross@outlook.com>
2021-03-07 16:27:48 -0500

    ok

---
+++ java/src/com/company/solutions/Test.java
@@ -0,0 +1,25 @@
+package com.company.solutions;
+
+
+/**
+ * @author Arturo Negreiros
+ */
+
+public class test {
+
+	/**
+	 * @param name
+	 * @param lastName
+	 * @param phoneNumber
+	 */
+
+	public void getParameters(String name, String lastName, String phoneNumber){
+
+	}
+
+
+
+	public static void main(String[] args) {
+		
+	}
+}
