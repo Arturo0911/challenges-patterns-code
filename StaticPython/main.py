868b00687bfbcbd3069293bdf23556302cb3d27d
Arturo0911 <anegreiross@outlook.com>
2021-08-01 02:42:13 -0500

    new commit

---
+++ StaticPython/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+import time
+import sys
+
+
+# examples types statics
+
+def show_time():
+    pass
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
