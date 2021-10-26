00a7c54faaed4cd6079ab08872c9ec771a47a3ed
Arturo0911 <anegreiross@outlook.com>
2021-10-25 23:33:16 -0500

    class logger implemented in singleton pattern

--- PythonChallenges/desing-patterns/singleton/logger.py
+++ PythonChallenges/desing-patterns/singleton/logger.py
@@ -3,39 +3,54 @@
 specially used to make the debugging
 more understandable"""
 
+from typing import Any
+
+
+
 
 # a simple solution to replace the print
 # statements to see the behavior of code
 
-def criticla(msg: str):
-    with open("filename.log", "a") as log_file:
-        log_file.write(f"[CRITICAL] {msg}\n")
+
+def write_log(filename: str, level: str, msg: str):
+    with open(filename) as log_file:
+        log_file.write(f"[{level}] {msg}\n")
 
 
-def error(msg: str):
-    with open("filename.log", "a") as log_file:
-        log_file.write(f"[ERROR] {msg}\n")
-
-
-def warn(msg: str):
-    with open("filename.log", "a") as log_file:
-        log_file.write(f"[WARN] {msg}\n")
-
-
-def info(msg: str):
-    with open("filename.log", "a") as log_file:
-        log_file.write(f"[INFO] {msg}\n")
-
-
-def debug(msg: str):
-    with open("filename.log", "a") as log_file:
-        log_file.write(f"[DEBUG] {msg}\n")
+def critical(msg: str):
+    write_log("CRITICAL", msg)
 
 
 
+class Logger:
+    """A file-based message logger with the following properties
 
-def log_message(msg: str) -> None:
-    with open("filename.log", "a") as log_file:
-        log_file.write(f"{msg}\n")
+    Attributes:
+        file_name: a string representing the full path of th log file to which
+        this logger will write its messages"""
 
+
+    def __init__(self, file_name: str):
+        """Return a logger object whote file_name is *file_name"""
+        self.file_name = file_name
+
+    def _write_log(self, level: str, msg: str):
+        """Writes a message to the file_name for a specific Logger instance"""
+        with open(self.file_name, "a") as log_file:
+            log_file.write(f"[{level}] {msg}\n")
+
+    def critical(self, level: str, msg: str):
+        self._write_log("CRITICAL", msg)
+    
+    def error(self, level: str,  msg: str):
+        self._write_log("ERROR", msg)
+
+    def warn(self, level: str, msg: str):
+        self._write_log("WARN", msg)
+
+    def info(self, level: str, msg: str):
+        self._write_log("INFO", msg)
+
+    def debug(self, level: str, msg: str):
+        self._write_log("DEBUG", msg)
 
