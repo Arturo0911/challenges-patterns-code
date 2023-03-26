275b1ea6523b1ab2262cc899dc0cfc045158e912
Arturo0911 <anegreiross@outlook.com>
2023-03-26 00:32:43 -0500

    new testing, with the models 7 and 6 we have an score of 80%

--- NeuralNetworks/main.py
+++ NeuralNetworks/main.py
@@ -3,7 +3,7 @@
 import os
 import tensorflow as tf
 import keras
-
+from pwn import *
 from tensorflow.python.keras.initializers import glorot_uniform
 from tensorflow.python.keras.models import (
     model_from_json,
@@ -16,28 +16,50 @@ from tensorflow.keras.preprocessing.image import (
 )
 from tensorflow.keras.utils import CustomObjectScope
 from PIL import Image
+from typing import Any
 
-def main():
+
+def handle_image(path: str) -> Any:
+    
+    image = Image.open(f"tests/{path}")
+    image = image.resize((224, 224))
+    x = img_to_array(image) /255.0
+    x = x.reshape(1, 224, 224, 3)
+    return x
+
+
+def accuracy_testing(path_model: int, 
+                     path_image: str):
     """
     Defining the path for the pre-trained models
     """
     MAIN_PATH = "models/"
-    model = f"{MAIN_PATH}riceaiv4.json"
-    weights = f"{MAIN_PATH}riceaiv4.hdf5"
+    model = f"{MAIN_PATH}riceaiv{path_model}.json"
+    weights = f"{MAIN_PATH}riceaiv{path_model}.hdf5"
     
+    log.progress(f"Loading model riceaiv{path_model} and loading {path_image}")
     
     with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
         with open(model, 'r') as f:
             model = model_from_json(f.read())
             model.load_weights(weights)
     
-    image = Image.open("tests/rice_test2.jpeg")
-    image = image.resize((224, 224))
-    x = img_to_array(image) / 255.0
-    x = x.reshape(1, 224, 224, 3)
+    x = handle_image(path_image)
     
     result = model.predict(x)
+    
+    
     print(result)
+    
+    
+def main():
+    try:
+        accuracy_testing(3, "rice_test4.jpeg")
+        accuracy_testing(7, "rice_test4.jpeg")
+        
+    except Exception as e:
+        log.error("error in the testing models or json")
+        print(str(e))
 
 
 if __name__ == "__main__":
